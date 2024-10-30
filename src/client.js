26ae485b6a7154d18f2f36dca3be8cac48647db5
Thomas <thomasmckenna12gmail.com>
2024-10-29 20:37:32 -0500

    successfully added a get request

--- src/client.js
+++ src/client.js
@@ -1,29 +1,41 @@
 const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 3000
+app.use(express.json())
 
-
-const mockUsers = [
-    { id: 1, username: "thomas", displayName: "Thomas"},
-    { id: 2, username: "joe", displayName: "Joe"},
-    { id: 3, username: "angie", displayName: "Angie"}
+// create a library full of books
+const books = [
+    {id: 1, title: 'Akata Witch', author: 'Nnedi Okorafor'}, 
+    {id: 2, title: 'Assassins Apprentice', author: 'Robin Hobb'}, 
+    {id: 3, title: 'The Blue Sword', author: 'Robin McKinley'}, 
+    {id: 4, title: 'City Of Stairs', author: 'Tom Rob'}, 
 ]
+// get a book with the name
+// user puts in a title of a book
+// search the books title 
+//if book title exists return book title
+// if not a title return error
 
-app.get("/", (request, response) => {
-    const test = "test"
-    response.send(test)
+app.get('/books/:title', (req, res) => {
+    const reqBook = req.params.title.toLocaleLowerCase() // what the user types in 
+    const searchedBook = books.find((book) => book.title.toLocaleLowerCase() === reqBook)
+    if (searchedBook) {
+        res.send(searchedBook)
+    } else {
+        return res.status(404).send('Invalid Book')
+    }
+    
 })
 
-app.get("/users/:id", (req, res) => {
-    const parsedId = parseInt(req.params.id);
-    if (isNaN(parsedId)) 
-        return res.status(400).send({ msg: "Bad Request. Invalid ID."})
-    const findUsers = mockUsers.find((user) => user.id === parsedId);
-    if (!findUsers) return res.sendStatus(404)
-    return res.send(findUsers)
+// post a book with a name?
+app.get('/books/:title', (res, req) => {
+    
 })
+
+// delete book with a name? 
+
+
 
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`)
 })
-
---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 3000
+app.get(express.json())
+
+const mockUsers = [
+    { id: 1, username: "thomas", displayName: "Thomas"},
+    { id: 2, username: "joe", displayName: "Joe"},
+    { id: 3, username: "angie", displayName: "Angie"}
+]
+
+app.post("/users", (req, res) => {
+    console.log(req.body)
+    res.send(200)
+})
+
+app.get("/", (request, response) => {
+    const test = "test"
+    response.send(test)
+})
+
+app.get("/users/:id", (req, res) => {
+    const parsedId = parseInt(req.params.id);
+    if (isNaN(parsedId)) 
+        return res.status(400).send({ msg: "Bad Request. Invalid ID."})
+    const findUsers = mockUsers.find((user) => user.id === parsedId);
+    if (!findUsers) return res.sendStatus(404)
+    return res.send(findUsers)
+})
+
+app.listen(PORT, () => {
+    console.log(`Running on port ${PORT}`)
+})
